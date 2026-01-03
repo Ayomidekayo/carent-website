1bdc6f499fae5649adb782d88099c4f78405c3ec
YourName <your-email@example.com>
2026-01-04 00:53:10 +0100

    updatee cors

--- server/server.js
+++ server/server.js
@@ -13,11 +13,24 @@ const app=express();
 //connect to database
 await connectDB();
 //middleware
+import cors from "cors";
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://carent-website-frontend.vercel.app"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173", // or your frontend URL
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
+
 
 app.use(express.json());
 
